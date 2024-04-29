c02fb115f12c541ff7f430f418f6a28e3c684b50
Bill <biliff@fsu.edu>
2024-04-29 20:24:32 +0000

    entered solution

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,11 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello World!" << endl;
+    return(0);
+}
